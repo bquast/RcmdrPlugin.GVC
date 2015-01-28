b04d11da8f2ac98b1f44d672f166f48001f3ab11
Bastiaan Quast <bquast@gmail.com>
2015-01-28 16:55:47 +0100

    setup function

--- R/RcmdrPlugin.decompr.R
+++ R/RcmdrPlugin.decompr.R
@@ -1,4 +1,4 @@
-#'
+#' Interface function for decompositions
 
 # loads Rcmdr library upon being loaded
 # copied for J. Fox's package: RcmdrPlugin.TeachingDemos (version 1.0-7)
@@ -22,3 +22,13 @@
   }
 }
 
+#' @export
+
+RcmdrPlugin.decompr.ltv <- function(){
+  require(TeachingDemos)
+  use.rgl <- options("Rcmdr")[[1]]$use.rgl
+  if (length(use.rgl) == 0 || use.rgl) require(rgl)
+  rgl.open()
+  rgl.coin()
+  flip.rgl.coin()
+}
