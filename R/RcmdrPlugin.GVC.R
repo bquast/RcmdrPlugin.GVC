76de96a3e47cb5b58c9a9453870636a5ccba9fb5
Bastiaan Quast <bquast@gmail.com>
2015-02-04 15:17:50 +0100

    working dialogue!!!!

--- R/RcmdrPlugin.GVC.R
+++ R/RcmdrPlugin.GVC.R
@@ -25,6 +25,57 @@
 
 gvc_decomp <- function(){
   
-  
+  defaults <- list (initial.x = NULL, initial.alternative = "two.sided", initial.level = ".95", 
+                    initial.mu = "0.0")
+  dialog.values <- getDialog ("singleSampleTTest", defaults)  
+  initializeDialog(title = gettextRcmdr("Single-Sample t-Test"))
+  xBox <- variableListBox(top, Numeric(), title = gettextRcmdr("Variable (pick one)"),
+                          initialSelection = varPosn(dialog.values$initial.x, "numeric"))
+  onOK <- function() {
+    x <- getSelection(xBox)
+    if (length(x) == 0) {
+      errorCondition(recall = singleSampleTTest, message = gettextRcmdr("You must select a variable."))
+      return()
+    }
+    alternative <- as.character(tclvalue(alternativeVariable))
+    level <- tclvalue(confidenceLevel)
+    mu <- tclvalue(muVariable)
+    putDialog ("singleSampleTTest", list (initial.x = x, initial.alternative = alternative, 
+                                          initial.level = level, initial.mu = mu))
+    closeDialog()
+    doItAndPrint(paste("with(", ActiveDataSet (), ", (t.test(", x, 
+                       ", alternative='", alternative, "', mu=", mu, ", conf.level=", 
+                       level, ")))", sep = ""))
+    tkdestroy(top)
+    tkfocus(CommanderWindow())
+  }
+  OKCancelHelp(helpSubject = "t.test", reset = "singleSampleTTest", apply = "singleSampleTTest")
+  optionsFrame <- tkframe(top)
+  radioButtons(optionsFrame, name = "alternative", buttons = c("twosided", 
+                                                               "less", "greater"), values = c("two.sided", "less", "greater"), 
+               labels = gettextRcmdr(c("Population mean != mu0", "Population mean < mu0", 
+                                       "Population mean > mu0")), title = gettextRcmdr("Alternative Hypothesis"),
+               initialValue = dialog.values$initial.alternative)
+  rightFrame <- tkframe(optionsFrame)
+  confidenceFrame <- tkframe(rightFrame)
+  confidenceLevel <- tclVar(dialog.values$initial.level)
+  confidenceField <- ttkentry(confidenceFrame, width = "6", 
+                              textvariable = confidenceLevel)
+  muFrame <- tkframe(rightFrame)
+  muVariable <- tclVar(dialog.values$initial.mu)
+  muField <- ttkentry(muFrame, width = "8", textvariable = muVariable)
+  tkgrid(getFrame(xBox), sticky = "nw")
+  tkgrid(labelRcmdr(rightFrame, text = ""), sticky = "w")
+  tkgrid(labelRcmdr(muFrame, text = gettextRcmdr("Null hypothesis: mu = ")), 
+         muField, sticky = "w", padx=c(10, 0))
+  tkgrid(muFrame, sticky = "w")
+  tkgrid(labelRcmdr(confidenceFrame, text = gettextRcmdr("Confidence Level: ")), 
+         confidenceField, sticky = "w", padx=c(10, 0))
+  tkgrid(confidenceFrame, sticky = "w")
+  tkgrid(alternativeFrame, rightFrame, sticky = "nw")
+  tkgrid(optionsFrame, sticky="w")
+  tkgrid(buttonsFrame, columnspan = 2, sticky = "w")
+  tkgrid.configure(confidenceField, sticky = "e")
+  dialogSuffix()
   
 }
